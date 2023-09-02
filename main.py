412e30152b8fa239abc0980530be3061c34fdbe4
JaazibTariq <Jaazibt@gmail.com>
2023-09-01 22:20:11 -0400

    Initialise

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 ##main
+import pygame, sys
+import random
+from pygame.locals import QUIT
+from pygame import mixer
+
+pygame.init()
+clock = pygame.time.Clock()
+width = 1000
+height = 600
+done = True
+screen = pygame.display.set_mode((width, height))
+
+while done:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
