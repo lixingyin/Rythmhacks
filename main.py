d0287f196d42c791fe2b24865270a110b7f88a83
IceAce2022 <114456809+IceAce2022@users.noreply.github.com>
2023-09-02 00:03:34 -0400

    init: WASD input

--- main.py
+++ main.py
@@ -14,5 +14,14 @@ screen = pygame.display.set_mode((width, height))
 while done:
     pygame.display.update()
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    print("Move the character forwards")
+                elif event.key == pygame.K_s:
+                    print("Move the character backwards")
+                elif event.key == pygame.K_a:
+                    print("Move the character left")
+                elif event.key == pygame.K_d:
+                    print("Move the character right")
         if event.type == QUIT:
             pygame.quit()
