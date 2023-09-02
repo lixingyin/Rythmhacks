a27c465f54c27361095f37652dc169182167f145
IceAce2022 <114456809+IceAce2022@users.noreply.github.com>
2023-09-02 01:03:19 -0400

    add: all keyboard inputs

--- main.py
+++ main.py
@@ -15,13 +15,57 @@ while done:
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    print("Move the character forwards")
-                elif event.key == pygame.K_s:
-                    print("Move the character backwards")
+                if event.key == pygame.K_q: 
+                    print("The Q button was pressed")
+                elif event.key == pygame.K_w:
+                    print("The W button was pressed")
+                elif event.key == pygame.K_e:
+                    print("The E button was pressed")
+                elif event.key == pygame.K_r:
+                    print("The R button was pressed")
+                elif event.key == pygame.K_t:
+                    print("The T button was pressed")
+                elif event.key == pygame.K_y:
+                    print("The Y button was pressed")
+                elif event.key == pygame.K_u:
+                    print("The U button was pressed")
+                elif event.key == pygame.K_i:
+                    print("The I button was pressed")
+                elif event.key == pygame.K_o:
+                    print("The O button was pressed")
+                elif event.key == pygame.K_p:
+                    print("The P button was pressed")
                 elif event.key == pygame.K_a:
-                    print("Move the character left")
+                    print("The A button was pressed")
+                elif event.key == pygame.K_s:
+                    print("The S button was pressed")
                 elif event.key == pygame.K_d:
-                    print("Move the character right")
+                    print("The D button was pressed")
+                elif event.key == pygame.K_f:
+                    print("The F button was pressed")
+                elif event.key == pygame.K_g:
+                    print("The G button was pressed")
+                elif event.key == pygame.K_h:
+                    print("The H button was pressed")
+                elif event.key == pygame.K_j:
+                    print("The J button was pressed")
+                elif event.key == pygame.K_k:
+                    print("The K button was pressed")
+                elif event.key == pygame.K_l:
+                    print("The L button was pressed")
+                elif event.key == pygame.K_z:
+                    print("The Z button was pressed")
+                elif event.key == pygame.K_x:
+                    print("The X button was pressed")
+                elif event.key == pygame.K_c:
+                    print("The C button was pressed")
+                elif event.key == pygame.K_v:
+                    print("The V button was pressed")
+                elif event.key == pygame.K_b:
+                    print("The B button was pressed")
+                elif event.key == pygame.K_n:
+                    print("The N button was pressed")
+                elif event.key == pygame.K_m:
+                    print("The M button was pressed")
         if event.type == QUIT:
             pygame.quit()
